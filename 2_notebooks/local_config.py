f67380866d6bb04b20eaf79155c5e2dad73c37e9
JenyaSkyt <jenya.skyt@gmail.com>
2026-04-15 13:54:59 +0300

    Remove local config from tracking

--- 2_notebooks/local_config.py
+++
@@ -1,9 +0,0 @@
-
-DB_USER = "postgres"
-DB_PASSWORD = "7777" 
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "amazon_sales"
-
-directory_path = '/Users/jenyaskitskaya/Documents/Portfolio/Amazon_Sales_Analisys_(Westbrook)'
-
