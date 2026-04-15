2ea455064355f72554187c02baa32ffcb0acf744
JenyaSkyt <jenya.skyt@gmail.com>
2026-04-15 13:35:41 +0300

    local config & Dashboard update

--- 2_notebooks/local_config.py
+++ 2_notebooks/local_config.py
@@ -5,20 +5,5 @@ DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_NAME = "amazon_sales"
 
+directory_path = '/Users/jenyaskitskaya/Documents/Portfolio/Amazon_Sales_Analisys_(Westbrook)'
 
-
-# Add at the beginning of each notebook
-import os
-os.chdir('/Users/jenyaskitskaya/Documents/Portfolio/Amazon_Sales_Analisys_(Westbrook)')
-
-# Doesn't work 
-PROJECT_DIR = '/Users/jenyaskitskaya/Documents/Portfolio/Amazon_Sales_Analisys_(Westbrook)'
-# Optional: set working directory to project root (local environment only)
-
-import os
-
-try:
-    from local_config import PROJECT_DIR
-    os.chdir(PROJECT_DIR)
-except ImportError:
-    pass
